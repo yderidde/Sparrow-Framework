e94c03d48d2f859527c02a7ff32ae967be53a6c3
Daniel Sperl <daniel@gamua.com>
2013-03-31 18:28:40 +0200

    enhanced render texture scene with colored brushes and erase mode

--- samples/demo/src/Classes/RenderTextureScene.m
+++ samples/demo/src/Classes/RenderTextureScene.m
@@ -20,6 +20,8 @@
 {
     SPRenderTexture *_renderTexture;
     SPImage *_brush;
+    SPButton *_button;
+    NSMutableDictionary *_colors;
 }
 
 - (id)init
@@ -33,17 +35,13 @@
 
 - (void)setupScene
 {
-    NSString *description = @"Touch the screen to draw sparrows!";
-    SPTextField *infoText = [SPTextField textFieldWithWidth:300 height:100 
-                                                       text:description fontName:@"Verdana" 
-                                                   fontSize:13 color:0x0];    
-    infoText.x = infoText.y = 10;
-    infoText.vAlign = SPVAlignTop;
-    infoText.touchable = NO;
-    [self addChild:infoText];
+    _colors = [[NSMutableDictionary alloc] init];
     
-    // we load the "brush" image (the sparrow egg) from the texture atlas
-    _brush = [[SPImage alloc] initWithContentsOfFile:@"benchmark_object.png"];
+    // we load the "brush" image from disk
+    _brush = [[SPImage alloc] initWithContentsOfFile:@"brush.png"];
+    _brush.pivotX = (int)(_brush.width / 2);
+    _brush.pivotY = (int)(_brush.height / 2);
+    _brush.blendMode = SP_BLEND_MODE_NORMAL;
     
     // the render texture is a dyanmic texture. We will draw the egg on that texture on
     // every touch event.
@@ -51,32 +49,75 @@
     
     // the canvas image will display the render texture
     SPImage *canvas = [SPImage imageWithTexture:_renderTexture];
-    
-    // we want to draw on touch events
-    [canvas addEventListener:@selector(onTouch:) atObject:self forType:SP_EVENT_TYPE_TOUCH];    
-    
+    [canvas addEventListener:@selector(onTouch:) atObject:self forType:SP_EVENT_TYPE_TOUCH];
     [self addChild:canvas];
+    
+    // we draw a text into that canvas
+    NSString *description = @"Touch the screen\nto draw!";
+    SPTextField *infoText = [SPTextField textFieldWithWidth:256 height:128
+                                                       text:description fontName:@"Verdana"
+                                                   fontSize:24 color:0x0];
+    infoText.x = CENTER_X - infoText.width / 2;
+    infoText.y = CENTER_Y - infoText.height / 2;
+    [_renderTexture drawObject:infoText];
+    
+    // add a button to let the user switch between "draw" and "erase" mode
+    SPTexture *buttonTexture = [[SPTexture alloc] initWithContentsOfFile:@"button_normal.png"];
+    _button = [[SPButton alloc] initWithUpState:buttonTexture text:@"Mode: Draw"];
+    _button.x = (int)(CENTER_X - _button.width / 2);
+    _button.y = 15;
+    [_button addEventListener:@selector(onButtonTriggered:) atObject:self
+                      forType:SP_EVENT_TYPE_TRIGGERED];
+    [self addChild:_button];
+}
+
+- (void)onButtonTriggered:(SPEvent *)event
+{
+    if (_brush.blendMode == SP_BLEND_MODE_NORMAL)
+    {
+        _brush.blendMode = SP_BLEND_MODE_ERASE;
+        _button.text = @"Mode: Erase";
+    }
+    else
+    {
+        _brush.blendMode = SP_BLEND_MODE_NORMAL;
+        _button.text = @"Mode: Draw";
+    }
 }
 
 - (void)onTouch:(SPTouchEvent *)event
-{    
+{
     NSSet *allTouches = [event touchesWithTarget:self];
     
-    for (SPTouch* touch in allTouches)
+    [_renderTexture drawBundled:^
     {
-        // don't draw on 'finger up'
-        if (touch.phase == SPTouchPhaseEnded) continue;
-        
-        // find out location of touch event
-        SPPoint *currentLocation = [touch locationInSpace:self];                
-        
-        // center brush over location
-        _brush.x = currentLocation.x - _brush.width / 2.0f;
-        _brush.y = currentLocation.y - _brush.height / 2.0f;
-        
-        // draw brush to render texture
-        [_renderTexture drawObject:_brush];        
-    } 
+        for (SPTouch* touch in allTouches)
+        {
+            NSNumber *touchID = @((uint)touch);
+            
+            // don't draw on 'finger up'
+            if (touch.phase == SPTouchPhaseEnded)
+            {
+                [_colors removeObjectForKey:touchID];
+                continue;
+            }
+            
+            if (touch.phase == SPTouchPhaseBegan)
+                _colors[touchID] = @([SPUtils randomIntBetweenMin:0 andMax:0xffffff]);
+            
+            // find out location of touch event
+            SPPoint *currentLocation = [touch locationInSpace:self];
+            
+            // center brush over location
+            _brush.x = currentLocation.x;
+            _brush.y = currentLocation.y;
+            _brush.color = [_colors[touchID] intValue];
+            _brush.rotation = [SPUtils randomFloat] * TWO_PI;
+            
+            // draw brush to render texture
+            [_renderTexture drawObject:_brush];
+        }
+    }];
 }
 
 @end
