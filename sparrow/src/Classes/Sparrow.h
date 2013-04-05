b5c248a20cfda9c94abc51f43d213e47d422f6fd
Daniel Sperl <daniel@gamua.com>
2013-04-05 15:13:36 +0200

    changed version define to '2.0rc'

--- sparrow/src/Classes/Sparrow.h
+++ sparrow/src/Classes/Sparrow.h
@@ -15,7 +15,7 @@
 #warning "This project uses features only available in iOS SDK 5.0 and later."
 #endif
 
-#define SPARROW_VERSION @"2.0"
+#define SPARROW_VERSION @"2.0rc"
 
 #import "SPNSExtensions.h"
 #import "SPEventDispatcher.h"
